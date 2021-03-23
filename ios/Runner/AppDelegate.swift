c2c16b2f7b57c7a0799b8225f534f8b8157b7a61
Lino <michi.banfi01@gmail.com>
2021-03-23 15:02:18 +0100

    ios config

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,13 @@
 import UIKit
 import Flutter
-
+import GoogleMaps
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCc-16mvBlbztZ44hjE2LJB1ZNvXbZrwGM")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/services/geolocator_service.dart
+++ lib/services/geolocator_service.dart
@@ -3,11 +3,14 @@ import 'package:permission_handler/permission_handler.dart';
 
 class GeolocatorService {
   static Future<PermissionStatus> getPermission() async {
+    //print('-----');
+    //print(await Permission.locationAlways.request());
     return await Permission.location.request();
   }
 
   static Future<Position> getLocation() async {
     return getPermission().then((result) async {
+      //print(Permission.locationAlways.request());
       if (result == PermissionStatus.granted) {
         //va gestita anche la parte iOS
         return await Geolocator.getCurrentPosition(
